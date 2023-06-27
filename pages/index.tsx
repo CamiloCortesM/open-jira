e6b6e7ccdf397515237d4f2cf79fecf7f19cac62
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-26 21:58:39 -0500

    Main screen layout - index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,39 @@
-import { Layout } from '@/components/layouts';
-import { Typography } from '@mui/material';
 import { NextPage } from 'next';
+import { Layout } from '@/components/layouts';
+import { Card, CardHeader, Grid } from '@mui/material';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1">Hello World</Typography>
+    <Layout title="Home - Openjira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: 'calc(100vh - 100px)',
+            }}
+          >
+            <CardHeader title="Pending" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: 'calc(100vh - 100px)',
+            }}
+          >
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: 'calc(100vh - 100px)',
+            }}
+          >
+            <CardHeader title="Completed" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
