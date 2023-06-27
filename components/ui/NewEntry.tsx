d07e500021b1cb9d8c252b5b176a5c3c41e9c585
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-26 23:24:22 -0500

    Working with our form and status

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,8 +1,18 @@
+import { ChangeEvent, useState } from 'react';
 import { Box, Button, TextField } from '@mui/material';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  const [inputValue, setInputValue] = useState('');
+  const [touched, setTouched] = useState(false);
+
+  const onTextFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <Box
       sx={{
@@ -10,34 +20,47 @@ export const NewEntry = () => {
         paddingX: 1,
       }}
     >
-      <Button
-        startIcon={<AddCircleOutlineOutlinedIcon />}
-        fullWidth
-        variant="outlined"
-      />
-
-      <TextField
-        fullWidth
-        sx={{
-          marginTop: 2,
-          marginBottom: 1,
-        }}
-        placeholder="New entry"
-        autoFocus
-        multiline
-        label="New Entry"
-        helperText="Enter the value"
-      />
-      <Box display="flex" justifyContent="space-between">
-        <Button variant="text">Cancel</Button>
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{
+              marginTop: 2,
+              marginBottom: 1,
+            }}
+            placeholder="New entry"
+            autoFocus
+            multiline
+            label="New Entry"
+            helperText={inputValue.length <= 0 && touched && 'Enter the value'}
+            error={inputValue.length <= 0 && touched}
+            value={inputValue}
+            onChange={onTextFieldChanged}
+            onBlur={() => setTouched(true)}
+          />
+          <Box display="flex" justifyContent="space-between">
+            <Button variant="text" onClick={() => setIsAdding(false)}>
+              Cancel
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              endIcon={<SaveOutlinedIcon />}
+            >
+              Save
+            </Button>
+          </Box>
+        </>
+      ) : (
         <Button
+          startIcon={<AddCircleOutlineOutlinedIcon />}
+          fullWidth
           variant="outlined"
-          color="secondary"
-          endIcon={<SaveOutlinedIcon />}
+          onClick={() => setIsAdding(true)}
         >
-          Save
+          Add Todo
         </Button>
-      </Box>
+      )}
     </Box>
   );
 };
