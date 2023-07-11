be18b67656809f1376ec3f5044876ee786ca7072
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-11 00:44:12 -0500

    Creating a Mongoose schematic

---
+++ models/Entry.ts
@@ -0,0 +1,21 @@
+import { Entry } from '@/interfaces';
+import mongoose, { Model, Schema } from 'mongoose';
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: { type: String, required: true },
+  createAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      values: ['pending', 'in-progress', 'finished'],
+      message: '{VALUE} is not allowed',
+    },
+  },
+});
+
+const EntryModel: Model<IEntry> =
+  mongoose.models.Entry || mongoose.model('Entry', entrySchema);
+
+export default EntryModel;
