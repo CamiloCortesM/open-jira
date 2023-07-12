10f857a85668d0f211d791e8e19bd759e85539cc
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-11 22:11:09 -0500

    Create a new entry - Backend

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -12,6 +12,8 @@ export default function handler(
     case 'GET':
       return getEntries(res);
 
+    case 'POST':
+      return createEntry(req, res);
     default:
       return res.status(400).json({
         message: 'Endpoint not exists',
@@ -20,9 +22,28 @@ export default function handler(
 }
 
 const getEntries = async (res: NextApiResponse<Data>) => {
-  db.connect();
+  await db.connect();
   const entries = await Entry.find().sort({ createdAt: 'ascending' });
-  db.disconnect();
+  await db.disconnect();
 
   return res.status(200).json(entries);
+};
+
+const createEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description = '' } = req.body;
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  });
+
+  try {
+    await db.connect();
+    await newEntry.save();
+    await db.disconnect();
+    res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    return res.status(500).json({ message: 'something went wrong' });
+  }
 };
