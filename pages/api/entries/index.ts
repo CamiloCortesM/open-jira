0866cfab99b2beb0744f382d3135dcca4a2e98c7
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-11 21:36:04 -0500

    Get all entries (endpoint)

---
+++ pages/api/entries/index.ts
@@ -0,0 +1,28 @@
+import { db } from '@/database';
+import { Entry, IEntry } from '@/models';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = { message: string } | IEntry[] | IEntry;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case 'GET':
+      return getEntries(res);
+
+    default:
+      return res.status(400).json({
+        message: 'Endpoint not exists',
+      });
+  }
+}
+
+const getEntries = async (res: NextApiResponse<Data>) => {
+  db.connect();
+  const entries = await Entry.find().sort({ createdAt: 'ascending' });
+  db.disconnect();
+
+  return res.status(200).json(entries);
+};
